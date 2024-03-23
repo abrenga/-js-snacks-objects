64a385d055cd56f2c07a986aef3dc1132ac5006d
a.brenga <antobrg90@hotmail.it>
2024-03-23 20:19:06 +0100

    feat: added the add Cards functionality

--- js/esercizioamodomio.js
+++ js/esercizioamodomio.js
@@ -6,16 +6,12 @@ const animals = [
 let btnAdd = document.getElementById("btnAdd");
 
 btnAdd.addEventListener("click", (e) => {
-    estrapolaDati()
+    estrapolaDati();
+    aggiungiCard();
 });
 
 
-function aggiungiCard() {
-    array.forEach(element => {
-        
-    });
 
-}
 
 
 function estrapolaDati() {
@@ -31,6 +27,36 @@ function estrapolaDati() {
 
     })
 
+}
+
+function aggiungiCard() {
+    svuotaCard();
+    animals.forEach(animal => {
+        createCardHTML(animal.nome, animal.famiglia, animal.classe);
+
+    });
+
+}
+
+
+function createCardHTML(nome, famiglia, classe) {
+    let colCard = document.getElementById("colCard");
+    colCard.innerHTML += `<div class="col">
+                               <div class="card">
+                                <button id="btnClose" type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast"
+                                    aria-label="Close"></button>
+                                <div class="card-body toast-container position-static">
+
+                                    <h5 class="card-title">${nome}</h5>
+                                    <p class="card-text">${famiglia}</p>
+                                    <p class="card-text">${classe}</p>
+                                </div>
+                                </div>`
+}
+
+function svuotaCard() {
+    let colCard = document.getElementById("colCard");
+    colCard.innerHTML = "";
 }
 
 
