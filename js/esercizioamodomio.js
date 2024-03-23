b1bf7d9fc1d2f66ba57236b2620a315e7700320f
Netta90 <58398245+Netta90@users.noreply.github.com>
2024-03-23 12:01:07 +0100

    feat: added js second version

---
+++ js/esercizioamodomio.js
@@ -0,0 +1,18 @@
+const animals = [
+    {
+        nome: "",
+        famiglia: "",
+        classe: ""
+    }
+];
+
+
+let btnAdd = document.getElementById("btnAdd");
+
+btnAdd.addEventListener("click", (e) => {
+  
+});
+
+
+
+   
