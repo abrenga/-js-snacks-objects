abf38ec6de95884528835760ad449bf7e294a3cd
a.brenga <antobrg90@hotmail.it>
2024-03-23 18:31:26 +0100

    feat:added the js btn

--- js/esercizioamodomio.js
+++ js/esercizioamodomio.js
@@ -1,18 +1,36 @@
 const animals = [
-    {
-        nome: "",
-        famiglia: "",
-        classe: ""
-    }
+
 ];
 
 
 let btnAdd = document.getElementById("btnAdd");
 
 btnAdd.addEventListener("click", (e) => {
-  
+    estrapolaDati()
 });
 
 
+function aggiungiCard() {
+    array.forEach(element => {
+        
+    });
 
-   
+}
+
+
+function estrapolaDati() {
+    let inputName = document.getElementById("inpNome");
+    let inputFam = document.getElementById("inpFam");
+    let inputClass = document.getElementById("inpClass");
+
+    animals.push({
+        nome: inputName.value,
+        famiglia: inputFam.value,
+        classe: inputClass.value
+
+
+    })
+
+}
+
+
